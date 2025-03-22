633d1c9fe44738a8b226388187db8472254c6335
Ana Genova <anagenova9@outlook.com>
2025-03-22 19:39:46 -0300

    Atualizando testes de reservas

--- src/test/java/br/com/fiap/reservas/infra/gateway/ReservaVMesaRepositorioJpaTest.java
+++ src/test/java/br/com/fiap/reservas/infra/gateway/ReservaVMesaRepositorioJpaTest.java
@@ -1,5 +1,8 @@
 package br.com.fiap.reservas.infra.gateway;
 
+import br.com.fiap.reservas.entities.ReservaVMesaEntity;
+import br.com.fiap.reservas.enums.StatusReserva;
+import br.com.fiap.reservas.infra.repository.mesa.MesaPK;
 import br.com.fiap.reservas.infra.repository.reserva.ReservaVMesa;
 import br.com.fiap.reservas.infra.repository.reserva.ReservaVMesaRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,36 +17,34 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 public class ReservaVMesaRepositorioJpaTest {
-//
-//    private ReservaVMesaRepositorioJpa reservaVMesaRepositorioJpa;
-//
-//    @Mock
-//    private ReservaVMesaRepository reservaVMesaRepository;
-//
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//        reservaVMesaRepositorioJpa = new ReservaVMesaRepositorioJpa(reservaVMesaRepository);
-//    }
-//
-//    @Test
-//    void deveBuscarReservaVMesaPeloId() {
-//        ReservaVMesa reservaMock = new ReservaVMesa(1L, "2023-10-10", "12:00", "14:00");
-//        when(reservaVMesaRepository.findById(anyLong())).thenReturn(Optional.of(reservaMock));
-//
-//        ReservaVMesa resultado = reservaVMesaRepositorioJpa.buscarReservaVMesaPeloId(1L);
-//
-//        assertNotNull(resultado);
-//        assertEquals("2023-10-10", resultado.getData());
-//        verify(reservaVMesaRepository).findById(anyLong());
-//    }
-//
-//    @Test
-//    void deveLancarExcecaoQuandoReservaVMesaNaoEncontrada() {
-//        when(reservaVMesaRepository.findById(anyLong())).thenReturn(Optional.empty());
-//
-//        RuntimeException exception = assertThrows(RuntimeException.class, () -> reservaVMesaRepositorioJpa.buscarReservaVMesaPeloId(1L));
-//
-//        assertEquals("ReservaVMesa não encontrada", exception.getMessage());
-//    }
+
+    private ReservaVMesaRepositorioJpa reservaVMesaRepositorioJpa;
+
+    @Mock
+    private ReservaVMesaRepository reservaVMesaRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        reservaVMesaRepositorioJpa = new ReservaVMesaRepositorioJpa(reservaVMesaRepository);
+    }
+
+    @Test
+    void deveCadastrarReservaVMesa() {
+        MesaPK mesaPK = new MesaPK(1L, 1);
+
+        ReservaVMesaEntity reservaEntity = new ReservaVMesaEntity(1L, 123L, mesaPK, StatusReserva.RESERVADA);
+
+        ReservaVMesa reservaMock = new ReservaVMesa(1L, 123L, mesaPK, StatusReserva.RESERVADA);
+        when(reservaVMesaRepository.save(any(ReservaVMesa.class))).thenReturn(reservaMock);
+        ReservaVMesaEntity resultado = reservaVMesaRepositorioJpa.cadastrarReservaVMesa(reservaEntity);
+
+        assertNotNull(resultado);
+        assertEquals(1L, resultado.getId());
+        assertEquals(123L, resultado.getIdReserva());
+        assertEquals(mesaPK, resultado.getIdMesa());
+        assertEquals(StatusReserva.RESERVADA, resultado.getStatus());
+
+        verify(reservaVMesaRepository, times(1)).save(any(ReservaVMesa.class));
+    }
 }
